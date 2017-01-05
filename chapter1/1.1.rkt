a9b1e8812945896f2252e4a283d669c401b804ee
Vittorio Dall'Acqua <vdallacqua@gmail.com>
2017-01-05 21:24:06 +0100

    1.1

--- chapter1/1.1.rkt
+++ chapter1/1.1.rkt
@@ -1,3 +1,5 @@
+; Below is a sequence of expressions. What is the result printed by the interpreter in response to each expression? Assume that the sequence is to be evaluated in the order in which it is presented.
+
 #lang sicp
 10 ; --> 10
 
