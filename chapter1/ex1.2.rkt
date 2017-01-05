d183dce5a0eff63ab65f7092255f227049bfbd8b
Vittorio Dall'Acqua <vdallacqua@gmail.com>
2017-01-05 22:55:07 +0100

    1.2

---
+++ chapter1/ex1.2.rkt
@@ -0,0 +1,16 @@
+;translate an expression in prefix form
+
+#lang sicp
+(/ (+ 5 4 (- 2
+              (- 3
+                  (+ 6
+                     (/ 4 5)
+                     )
+                  )
+             )
+      )
+   (* 3
+      (- 6 2)
+      (- 2 7)
+      )
+   )
